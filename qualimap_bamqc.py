cd9462a624883716e8b57dbf2a46b47188112bac
Scott Ouellette <scottx611x@gmail.com>
2018-07-27 12:20:42 -0400

    UPdate backing python script to move outputs to their proper location

--- qualimap_bamqc.py
+++ qualimap_bamqc.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import argparse
+import os
 from subprocess import check_call, CalledProcessError
+import shutil
 import sys
-import logging
-
-log = logging.getLogger(__name__)
 
 
 def qualimap_bamqc(bam_filename, genomecov_file, out_dir, jv_mem_size):
@@ -28,12 +27,16 @@ def main():
         description="Generate Bam Quality Statistics"
     )
     parser.add_argument('--input_file')
-    parser.add_argument('--out_genome_file', default="genome_coverage.txt")
+    parser.add_argument('--out_genome_file')
     parser.add_argument('--out_dir')
-    parser.add_argument('--java_mem_size', default="8G")
+    parser.add_argument('--out_zip')
+    parser.add_argument('--out_html')
+    parser.add_argument('--out_results')
+    parser.add_argument('--java_mem_size')
 
     args = parser.parse_args()
-    
+    print(args)
+
     qualimap_bamqc(
         args.input_file,
         args.out_genome_file,
@@ -41,6 +44,21 @@ def main():
         args.java_mem_size
     )
 
+    shutil.make_archive(
+        'raw_data_qualimapReport',
+        'zip',
+        os.path.join(args.out_dir, 'raw_data_qualimapReport')
+    )
+
+    shutil.move("raw_data_qualimapReport.zip", args.out_zip)
+    shutil.move(
+        os.path.join(args.out_dir, "genome_results.txt"),
+        args.out_results
+    )
+    shutil.move(
+        os.path.join(args.out_dir, "qualimapReport.html"),
+        args.out_html
+    )
 
 if __name__ == "__main__":
     main()
